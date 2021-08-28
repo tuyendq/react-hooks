34515637255f9db61d31a2cbe92d48d7ecc5f652
Tuyen Dang <tuyendq@gmail.com>
2021-08-28 18:01:24 +0700

    02_03_checkbox_with_useState: End

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 function App() {
+  const [checked, setChecked] = useState(false);
   return (
     <>
-      <h1>Hello React!</h1>
+      <h1>Checkbox with useState</h1>
+      <input type='checkbox' value={checked} onChange={() => setChecked((checked) => !checked)}/>
+      <p>{(checked) ? "checked" : "Not checked"}</p>
     </>
   );
 }
