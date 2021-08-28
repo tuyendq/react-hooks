cd804d9c13ddc9ff93a856ef448e9b19f23c2e18
Tuyen Dang <tuyendq@gmail.com>
2021-08-28 18:40:03 +0700

    02_05_components_tree: End

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { FaStar } from 'react-icons/fa';
@@ -7,17 +7,28 @@ const createArray = (length) => [
   ...Array(length)
 ];
 
-function Star() {
+function Star({ selected = false, onSelect }) {
   return (
-    <FaStar />
+    <FaStar 
+      color={selected ? 'red' : 'gray'}
+      onClick={onSelect}
+    />
   );
 }
 
 function StarRating({totalStars = 5}) {
+  const [selectedStars, setSelectedStars] = useState(0);
   return (
-    createArray(totalStars).map((n, i) => (
-      <Star key={i} />
-    ))
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star 
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>{selectedStars} of {totalStars}</p>
+    </>
   );
 }
 
